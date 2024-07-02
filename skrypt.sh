9db48eb1248ee5ded5ddb08df7c086c83db60161
Matched2003 <mateuszmusial2003@gmail.com>
2024-07-02 21:46:36 +0200

    Add short flags -d, -l, -h

--- skrypt.sh
+++ skrypt.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 case "$1" in
-	--date)
+	--date|-d)
 		echo "Dzisiaj jest: $(date)"
 		;;
-	--logs)
+	--logs|-l)
 		num_logs=${2:-100}
 		mkdir -p logs
 		for i in $(seq 1 $num_logs); do
 			echo -e "log${i}.txt\nskrypt.sh\n$(date)" > logs/log${i}.txt
 		done
 		;;
-	--help)
+	--help|-h)
 		echo "Dostępne opcje:"
-		echo "--date"
-		echo "--logs [number]"
-		echo "--help"
+		echo "--date, -d"
+		echo "--logs [number], -l [number]"
+		echo "--help, -h"
 		;;
 	*)
 		echo "Nieznana opcja. Użyj --help, aby zobaczyć dostępne opcje."
