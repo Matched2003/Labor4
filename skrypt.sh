3450e2ba4ae88c72233162e27ce346ea231783c1
Matched2003 <mateuszmusial2003@gmail.com>
2024-07-02 21:40:12 +0200

    Add --logs option

--- skrypt.sh
+++ skrypt.sh
@@ -4,6 +4,13 @@ case "$1" in
 	--date)
 		echo "Dzisiaj jest: $(date)"
 		;;
+	--logs)
+		num_logs=${2:-100}
+		mkdir -p logs
+		for i in $(seq 1 $num_logs); do
+			echo -e "log${i}.txt\nskrypt.sh\n$(date)" > logs/log${i}.txt
+		done
+		;;
 	*)
 		echo "Nieznana opcja. Użyj --help, aby zobaczyć dostępne opcje."
 		;;
